29a87bd65079684dd224515ab469a6dfbfbf4f48
herazocatalina77-svg <herazocatalina77@gmail.com>
2025-09-25 10:55:07 -0500

    TallerCondicionalesPunto2

---
+++ Potenciadeunnumero/Potenciadeunnumero/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Potenciadeunnumero1
+{
+    internal class Potenciadeunnumero
+    {
+        static void Main(string[] args)
+        {
+            double baseNum = 2;
+            double exponente = 3;
+            Console.WriteLine("Ingrese una base para la potencia");
+            baseNum = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ingrese un exponente para la potencia");
+            baseNum = Convert.ToDouble(Console.ReadLine());
+            double resultado = Math.Pow(baseNum, exponente);
+            Console.WriteLine($"El resultado de su potencia es: {resultado}");
+        }
+    }
+}
---
+++ TallerCondicionalesPunto2/TallerCondicionalesPunto2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Tiki
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Eres el personaje de un juego, tienes la posibilidad de disparar pero primero miremos que tantas municiones tienes");
+        int numero;
+        Random rnd = new Random();
+        numero = rnd.Next(1, 6);//Devuelve un número entre 0 y 5 
+        Console.WriteLine(numero);
+        Console.WriteLine("Inserta I si eres Invencible");
+        char letra = Console.ReadLine()[0];
+        if (letra == 'i' && numero > 1)
+        {
+            Console.WriteLine($"El jugador está disparando");
+        }
+        else
+        {
+            Console.WriteLine($"Pailas, vuelva a compilar el codigo");
+        }
+    }
+}
